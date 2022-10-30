c0f6be6e33a9af40cb82ed3d1dbdfa962e0f368c
hee <jkgt0521@naver.com>
2022-10-30 19:08:09 +0900

    Feat: #1 Spring Boot Project Setting

---
+++ src/main/java/com/trip/trip_in_world/TripInWorldBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trip.trip_in_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripInWorldBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripInWorldBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trip/trip_in_world/TripInWorldBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trip.trip_in_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripInWorldBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
