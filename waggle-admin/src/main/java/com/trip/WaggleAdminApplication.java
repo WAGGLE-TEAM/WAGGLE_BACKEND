298f05f56020a0cfa64885682cd191fcdfed1606
yu-heejin <jkgt0521@naver.com>
2023-08-06 00:29:35 +0900

    feat: 멀티모듈 의존성 설정 및 테스트 완료 [#16]

--- waggle-admin/src/main/java/com/trip/WaggleAdminApplication.java
+++
@@ -1,9 +0,0 @@
-package com.trip;
-
-@SpringBootApplication
-public class WaggleAdminApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(TripInWorldBackendApplication.class, args);
-    }
-}
--- waggle-api/src/main/java/com/trip/Main.java
+++
@@ -1,7 +0,0 @@
-package com.trip;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
