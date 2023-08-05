9c7e136a0070f8d74c2a3148141fa3b167f959a1
yu-heejin <jkgt0521@naver.com>
2023-08-05 21:28:58 +0900

    chore: 멀티 모듈 dependencies 수정 [#16]

---
+++ waggle-admin/src/main/java/com/trip/WaggleAdminApplication.java
@@ -0,0 +1,9 @@
+package com.trip;
+
+@SpringBootApplication
+public class WaggleAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripInWorldBackendApplication.class, args);
+    }
+}
