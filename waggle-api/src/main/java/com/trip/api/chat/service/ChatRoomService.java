1e5837e25a3d6c67ae4b683de62ff181cbfaf458
yu-heejin <jkgt0521@naver.com>
2023-09-17 22:26:37 +0900

    :sparkles:  feat: 채팅 멤버 insert 시 bulk insert 사용 [#34]

--- waggle-api/src/main/java/com/trip/api/chat/service/ChatRoomService.java
+++ waggle-api/src/main/java/com/trip/api/chat/service/ChatRoomService.java
@@ -7,8 +7,13 @@ import com.trip.api.chat.repository.ChatRoomMemberRepository;
 import com.trip.api.chat.repository.ChatRoomQueryDslRepository;
 import com.trip.api.chat.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -19,10 +24,27 @@ public class ChatRoomService {
     private final ChatRoomQueryDslRepository chatRoomQueryDslRepository;
     private final ChatRoomMemberRepository chatRoomMemberRepository;
     private final ChatRoomMapper chatRoomMapper;
+    private final JdbcTemplate jdbcTemplate;
 
     public Long createChatRoom(CreateChatRoomRequest chatRoomRequest) {
         ChatRoom chatRoom = chatRoomMapper.convertRequestDtoToEntity(chatRoomRequest);
+        List<Integer> joinUsers = chatRoomRequest.getJoinUsers();
+
         Long chatRoomId = chatRoomRepository.save(chatRoom).getId();
+        jdbcTemplate.batchUpdate("INSERT INTO chat_room_member (chat_room_id, member_id, is_exited) VALUES(" + chatRoomId + ", CAST(? AS bigint), true)",
+                new BatchPreparedStatementSetter() {
+                    @Override
+                    public void setValues(PreparedStatement ps, int i) throws SQLException {
+                        ps.setString(1, String.valueOf(joinUsers.get(i)));
+                    }
+
+                    @Override
+                    public int getBatchSize() {
+                        return joinUsers.size();
+                    }
+                }
+        );
+
         return chatRoomId;
     }
 
