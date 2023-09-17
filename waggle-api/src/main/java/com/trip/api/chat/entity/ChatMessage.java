6fbb4a47ce4c01b5a26f473ac08a308af9eb6136
yu-heejin <jkgt0521@naver.com>
2023-09-17 15:45:58 +0900

    :sparkles:  feat: chat_message Entity 추가 [#34]

--- waggle-api/src/main/java/com/trip/api/chat/entity/ChatMessage.java
+++ waggle-api/src/main/java/com/trip/api/chat/entity/ChatMessage.java
@@ -5,20 +5,27 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-@Entity
 @Getter
+@Entity
 @NoArgsConstructor
-@SQLDelete(sql = "UPDATE chat_room SET is_deleted = true WHERE id = ?")
-public class ChatRoom {
+@SQLDelete(sql = "UPDATE chat_message SET is_deleted = true WHERE id = ?")
+public class ChatMessage {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
-    private String name;
+    @Column(name = "member_id", nullable = false)
+    private Long memberId;
+
+    @Column(name = "chat_room_id", nullable = false)
+    private Long chatRoomId;
+
+    @NotBlank
+    private String message;
 
     @Column(name = "is_deleted", nullable = false)
     private Boolean isDeleted;
