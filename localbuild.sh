dd4e7436a3efe3550d44ff9cb20dbb8a8e1ea9ab
sylee <sylee6529@gmail.com>
2023-10-03 19:29:53 +0900

    :sparkles: feat: Build, Run 하는 Bash Script 작성 [#35]

---
+++ localbuild.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source ./.env
+
+./gradlew clean build
+cd waggle-api
+java -Djarmode=layertools -jar build/libs/"${JAR_FILE_NAME}".jar extract
+cd ..
+docker-compose up -d --build
+docker-compose up
