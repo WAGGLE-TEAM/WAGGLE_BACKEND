9f688cf2a7ffd585cdd8bc7d9f314dcb7a765f7f
yu-heejin <jkgt0521@naver.com>
2023-10-13 23:01:56 +0900

    :recycle:  refactor: request dto validate 메세지 추가 및 joinUsers 검증 삭제 [#34]

--- waggle-api/src/main/java/com/trip/api/chatting/dto/request/CreateChatMessageRequest.java
+++ waggle-api/src/main/java/com/trip/api/chatting/dto/request/CreateChatMessageRequest.java
@@ -9,9 +9,9 @@ import javax.validation.constraints.NotBlank;
 @Getter
 public class CreateChatMessageRequest {
 
-    @NotBlank
+    @NotBlank(message = "빈 메세지를 보낼 수 없습니다.")
     private String message;
 
-    @ValidateType(enumClass = MessageType.class)
+    @ValidateType(enumClass = MessageType.class, message = "올바른 메세지 타입이 아닙니다.")
     private String messageType;
 }
