118a96789b5159734dee2d79284050cedefb035e
yu-heejin <jkgt0521@naver.com>
2023-12-04 00:35:32 +0900

    :bug:  fix: createdAt, updatedAt null 값 오류 수정

--- waggle-api/src/main/java/com/trip/api/WaggleApiApplication.java
+++ waggle-api/src/main/java/com/trip/api/WaggleApiApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @Generated
 @ComponentScan(basePackages = { "com.trip" })
 @EntityScan(basePackages = { "com.trip" })
+@EnableJpaAuditing
 public class WaggleApiApplication {
 
     public static void main(String[] args) {
